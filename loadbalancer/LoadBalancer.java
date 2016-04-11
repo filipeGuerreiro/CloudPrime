58a3a9042406ed4076d7a889c971324dc4c8449f
FilipeGuerreiro <filipe.m.guerreiro@gmail.com>
2016-04-11 13:48:21 +0100

    added skeleton for mss

--- loadbalancer/LoadBalancer.java
+++ loadbalancer/LoadBalancer.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -9,27 +13,37 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 import java.util.concurrent.Executor;
 
 public class LoadBalancer {
     
-    private List<Integer> machinesInCluster;
+    private static List<String> machinesInCluster = Collections.synchronizedList(new ArrayList<String>());
     
     public LoadBalancer() {
-        machinesInCluster = new ArrayList<Integer>();
+        //machinesInCluster = new ArrayList<String>();
     }
 
     public static void main(String[] args) throws Exception {
         // open socket on port 8000 which expects requests as <IP>:8000/f.html?n=<requestNumber>
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/", new ReceiveRequestHandler());
-        System.out.println("Started loadbalancer at port 8000");
+        System.out.println("LB: Receiving requests at port 8000");
         
         // create a multi-threaded executor
-        Executor myExecutor = new ThreadPerTaskExecutor();
-        server.setExecutor(myExecutor); 
+        server.setExecutor(new ThreadPerTaskExecutor());
         
         server.start();
+        
+        // open socket on port 8001 which expects new webserver announcements as <IP>:8001/webserver/ip?<myIP>
+        HttpServer ipServer = HttpServer.create(new InetSocketAddress(8001), 0);
+        ipServer.createContext("/webserver", new WebserverIpHandler());
+        System.out.println("LB: Receiving webserver IPs at port 8001");
+        
+        // create a multi-threaded executor
+        ipServer.setExecutor(new ThreadPerTaskExecutor()); 
+        
+        ipServer.start();
     }
     
     // Executor which creates a thread for each request
@@ -57,7 +71,7 @@ public class LoadBalancer {
             // send request to machine
             String response = "NOT FOUND";
             try {
-                response = sendGet(machineIP, request)
+                response = sendGet(machineIP, request);
             } catch(Exception e) { System.out.println("Failed during request to webserver: " + e.toString()); }
             
             // send response to requester
@@ -69,13 +83,36 @@ public class LoadBalancer {
         }
     }
     
+    
+    static class WebserverIpHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            
+            // read request data
+            URI requestedUri = t.getRequestURI();
+            String request = requestedUri.getPath().split("/webserver/")[1];
+            
+            System.out.println("New IP: "+request);
+            // store IP name
+            addWebserver(request);
+            
+            String response = "OK";
+            // send response to requester
+            t.sendResponseHeaders(200, response.length());          
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            
+        }
+    }
+    
 
     public static String chooseMachine(String request) {
         
         return "TODO";
     }
     
-    private String sendGet(String machineIP, String request) throws Exception {
+    private static String sendGet(String machineIP, String request) throws Exception {
 
 		String url = machineIP+":8000/f.html?n="+request;
 		
@@ -84,9 +121,6 @@ public class LoadBalancer {
 
 		// optional default is GET
 		con.setRequestMethod("GET");
-
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
 
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'GET' request to URL : " + url);
@@ -104,8 +138,15 @@ public class LoadBalancer {
 
 		//print result
 		System.out.println(response.toString());
-        return StringBuffer.toString();
+        return response.toString();
 
 	}
+    
+    private static void addWebserver(String webserverIP) {
+        
+        synchronized(machinesInCluster) {
+            machinesInCluster.add(webserverIP);
+        }
+    }
 
 }
---
+++ mss/MSS.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.concurrent.Executor;
+
+public class MSS {
+    
+    private static List<String> machinesInCluster = Collections.synchronizedList(new ArrayList<String>());
+    
+    public MSS() {
+        //machinesInCluster = new ArrayList<String>();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // requests as <this_ip>:8010/<server_ip>/metric?<metric>
+        HttpServer server = HttpServer.create(new InetSocketAddress(8010), 0);
+        server.createContext("/", new ReceiveMetricsHandler());
+        System.out.println("LB: Receiving metrics at port 8010");
+        
+        // create a multi-threaded executor
+        server.setExecutor(new ThreadPerTaskExecutor());
+        
+        server.start();
+    }
+    
+    // Executor which creates a thread for each request
+    static class ThreadPerTaskExecutor implements Executor {
+        public void execute(Runnable r) {
+            new Thread(r).start();
+        }
+    }
+
+    static class ReceiveMetricsHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            
+            // read request data
+            URI requestedUri = t.getRequestURI();
+            String webserver = requestedUri.getPath().split("/")[1];
+            String request = requestedUri.getQuery();
+            
+            // TODO
+            System.out.println("webserver:" +webserver+" metric:"+request);
+
+            // send response to requester
+            t.sendResponseHeaders(200, response.length());
+            
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
